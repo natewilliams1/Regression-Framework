ab86beb7ad85c588ec13357a1319f2bcc8b9f9ea
Butch Mayhew <bmayhew@daxko.com>
2017-01-24 15:23:33 -0600

    Adding headless to suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,27 @@
 $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
 
 require 'rspec'
-require 'watir-webdriver'
+require 'watir'
 require 'page-object'
 require 'page-object/page_factory'
 require 'faker'
 require 'pages'
+require 'headless'
 
-browser = Watir::Browser.new :chrome
+
+
+headless = Headless.new
+headless.start
+
+browser = Watir::Browser.new :firefox
 browser.window.maximize
 
 RSpec.configure do |config|
   config.include PageObject::PageFactory
   config.before(:all) { @browser = browser }
-  #config.after(:suite) { browser.close }
+  #config.after(:suite) {  }
+  config.before(:suite) do
+   	browser.close
+   	headless.destroy
+  end
 end
