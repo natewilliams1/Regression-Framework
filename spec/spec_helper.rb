1d1e09ed3965cfe46fc0682e8687f207998d5575
Butch Mayhew <bmayhew@daxko.com>
2017-01-24 15:39:53 -0600

    Updating specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,27 @@
 $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
 
 require 'rspec'
-require 'watir'
+require 'watir-webdriver'
 require 'page-object'
 require 'page-object/page_factory'
 require 'faker'
 require 'pages'
-require 'headless'
-
+#require 'headless'
 
 
 headless = Headless.new
 headless.start
 
-browser = Watir::Browser.new :firefox
+
+browser = Watir::Browser.new :chrome
 browser.window.maximize
 
 RSpec.configure do |config|
   config.include PageObject::PageFactory
   config.before(:all) { @browser = browser }
-  #config.after(:suite) {  }
-  config.before(:suite) do
+  #config.after(:suite) { browser.close }
+  config.after(:suite) do
    	browser.close
-   	headless.destroy
+    headless.destroy
   end
 end
